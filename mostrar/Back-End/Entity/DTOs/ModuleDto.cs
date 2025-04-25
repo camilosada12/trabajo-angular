a05a5156e1d172d29c9f396f8a5751f7c75b0363
brayan <brayanperdomo0720@gmail.com>
2025-04-25 11:37:10 -0500

    sin carpeta

--- mostrar/Back-End/Entity/DTOs/ModuleDto.cs
+++
@@ -1,9 +0,0 @@
-﻿public class ModuleDto
-{
-    public int id { get; set; }
-    public string name { get; set; }
-    public string description { get; set; }
-    //public DateTime createddate { get; set; }
-    public bool active { get; set; }
-    //public bool isdeleted { get; set; }
-}
