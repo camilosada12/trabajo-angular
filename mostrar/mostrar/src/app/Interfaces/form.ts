d16cc89d7d68e04c7216a1ebfa5eda941d742c18
JesusDavid1405 <jesusdavidfierrorivera817@gmail.com>
2025-04-26 11:51:47 -0500

    ya esta user

--- mostrar/mostrar/src/app/Interfaces/form.ts
+++
@@ -1,8 +0,0 @@
-export interface Form {
-
-    id: number ;
-    name : String;
-    description : String; 
-    active: boolean; 
-    
-}
--- mostrar/mostrar/src/environments/environment.development.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    apiUrl: 'https://localhost:7205/api'  // normalmente pondrías aquí la URL real del back
-  };
-  
